484f163a2b6a021405197e21dbaaaaf6fbef01db
lucas <lucasluzfeitosa243@gmail.com>
2020-04-17 15:22:01 -0300

    correcao retorno da funcao de autenticacao

--- blueprints/login.py
+++ blueprints/login.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response, current_app
 import models
 from utils.error_handler import UnauthorizedException
 
 bp = Blueprint('login', __name__)
+
+
+def return_no_content():
+    response = make_response('', 204)
+    response.mimetype = current_app.config['JSONIFY_MIMETYPE']
+    return response
 
 
 @bp.route('/auth-proxy', methods=['GET'])
@@ -13,3 +19,5 @@ def auth():
     account = models.Accounts.query.filter_by(api_key=api_key).first()
     if not account:
         raise UnauthorizedException(message='API KEY invalida')
+    print('!!! Conectado')
+    return return_no_content()
