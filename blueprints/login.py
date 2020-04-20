04a63e9676d0ff2e0c687441b1fae0f3e78b7ffa
lucas <lucasluzfeitosa243@gmail.com>
2020-04-20 15:31:48 -0300

    tornando compativel com uWSGI

--- blueprints/login.py
+++ blueprints/login.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, request, make_response, current_app
-import models
+import requests
+from urllib.parse import urljoin
 from utils.error_handler import UnauthorizedException
 
 bp = Blueprint('login', __name__)
 
+KOPA_LIVE_DOMAIN = os.environ.get()
+session = requests.Session
 
 def return_no_content():
     response = make_response('', 204)
@@ -13,11 +16,10 @@ def return_no_content():
 
 @bp.route('/auth-proxy', methods=['GET'])
 def auth():
-    api_key = request.headers.get('x-api-key')
-    if not api_key:
-        raise UnauthorizedException(message='API KEY invalida')
-    account = models.Accounts.query.filter_by(api_key=api_key).first()
-    if not account:
-        raise UnauthorizedException(message='API KEY invalida')
-    print('!!! Conectado')
-    return return_no_content()
+    authorization = request.headers.get('Authorization')
+    url = urljoin(KOPA_LIVE_DOMAIN, 'auth/me')
+    res = session.get(url, headers={'Authorization': authorization})
+    if res.ok:
+        return return_no_content()
+    else:
+        raise UnauthorizedException(message='Token invalido')
--- core.py
+++ core.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import os
-import models
 from blueprints import videos, login
 import utils
 
@@ -9,27 +8,13 @@ def create_app(config: dict = None) -> dict:
     ''' core function to create a app '''
     app = Flask(__name__)
     app.debug = False
-    app.config.from_mapping(
-        SQLALCHEMY_DATABASE_URI='{}://{}:{}@{}/{}'.format(
-            os.environ.get('DB_CONNECTOR'),
-            os.environ.get('DB_USERNAME'),
-            os.environ.get('DB_PASSWORD'),
-            os.environ.get('DB_HOST'),
-            os.environ.get('DATABASE')
-        ),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
     if config:
         app.config.from_mapping(**config)
-    models.db.init_app(app=app)
-    models.migrate.init_app(app=app)
     utils.error_handler.error_treatment(app=app)
 
     # NOTE cadastro blueprints
     app.register_blueprint(videos.bp)
     app.register_blueprint(login.bp)
-
-    models.db.create_all(app=app)
 
     @app.after_request
     def after_request(response):
